410204d2177efe9b9b79b06e47362da84ce995c6
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-08-22 23:22:50 +0700

    :rocket: Create MessageService and inject into Heroes service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   /** Original 
   getHeroes(): Hero[] {
@@ -18,7 +19,8 @@ export class HeroService {
 
   /** Observable */
   getHeroes(): Observable<Hero[]> {
-    console.log(of(HEROES));
+      // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
