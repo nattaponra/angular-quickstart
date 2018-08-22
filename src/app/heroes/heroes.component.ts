11f90a50adf254eebe5f9a2944119284a4393c56
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-08-22 22:45:38 +0700

    master

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+
+ 
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {
+  
+   }
+
+  
+   getHeroes():void {
+     this.heroes = this.heroService.getHeroes();
+     console.log(this.heroes);
+   }
+
+   ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log(hero);
+  }
+
+
+  
+}
